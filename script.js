3b54d787fa3e559974ca4913b509b6acd844f23d
Oracle <rlfoster2018@gmail.com>
2020-07-15 17:04:26 -0500

    Final commitment

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            const bio = document.getElementById("bio");
+
+            for (var i = 0; i < json.length; i++) {
+                bio.innerHTML += `
+                <div>
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in Space: ${json[i].hoursInSpace}</li>
+                            <li>Active: ${json[i].active}</li>
+                            <li>Skills: ${json[i].skills}</li>
+                        </ul>
+                        
+                </div>
+            `
+            }; // blank space above /div is reserved for the image file
+        });
+    });
+});
